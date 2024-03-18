f9732b0417f946a13d5a4ee89144e704981a6383
FrancescoDAmbrosio1 <ciccio.dambrosio@hotmail.it>
2024-03-18 21:06:16 +0100

    ADD: ticket calculation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log('ciao')
+const kmInputElement = document.getElementById('km')    //object | null
+const userAgeElement = document.getElementById('userAge')   //object | null
+const buttonElement = document.getElementById('submit')     //object | null
+const bigliettoElement = document.getElementById('biglietto')   //object
+buttonElement.addEventListener('click', function(){
+   const km = parseFloat(kmInputElement.value)  //number
+   const age = parseFloat(userAgeElement.value) //number
+   let price = km * 0.21   //number
+   if (age < 18){
+        const discount20 = price * 0.20 //number
+        let discountPrice = price - discount20 //number
+        bigliettoElement.innerHTML = `<div>Il costo del biglietto è di ${discountPrice.toFixed(2)} &#8364 al quale è stato applicato uno sconto del 20% </div>`
+   } else if (age > 65){
+    const discount20 = price * 0.20 //number
+    let discountPrice = price - discount20 //number
+    bigliettoElement.innerHTML = `<div>Il costo del biglietto è di ${discountPrice.toFixed(2)} &#8364 al quale è stato applicato uno sconto del 40% </div>`
+
+   } else {
+    bigliettoElement.innerHTML = `<div>Il costo del biglietto è di ${price.toFixed(2)} &#8364 al quale non è stato applicato nessuno sconto</div>`
+
+   }
+})
