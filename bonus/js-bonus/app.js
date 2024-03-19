ecd9ede39ce66225b120c19032b45e9f709cc171
FrancescoDAmbrosio1 <ciccio.dambrosio@hotmail.it>
2024-03-19 02:26:35 +0100

    ADD: form and select

--- bonus/js-bonus/app.js
+++ bonus/js-bonus/app.js
@@ -1,22 +1,86 @@
 const kmInputElement = document.getElementById('km')    //object | null
-const userAgeElement = document.getElementById('userAge')   //object | null
-const buttonElement = document.getElementById('submit')     //object | null
-const bigliettoElement = document.getElementById('biglietto')   //object
-buttonElement.addEventListener('click', function(){
-   const km = parseFloat(kmInputElement.value)  //number
-   const age = parseFloat(userAgeElement.value) //number
+const userAgeElement = document.querySelector('#userAge') //object | null
+const formElement = document.getElementById('form-submit')    //object | null
+const bigliettoElement = document.getElementById('biglietto')
+
+console.log(typeof(userAgeElement))
+
+formElement.addEventListener('submit', function(event) {
+   event.preventDefault() 
+   const km = parseFloat(kmInputElement.value)//number
+   const age = parseInt(userAgeElement.value)
    let price = km * 0.21   //number
-   if (age < 18){
+   if (age == 1){
         const discount20 = price * 0.20 //number
         let discountPrice = price - discount20 //number
-        bigliettoElement.innerHTML = `<div>Il costo del biglietto è di ${discountPrice.toFixed(2)} &#8364 al quale è stato applicato uno sconto del 20% </div>`
-   } else if (age > 65){
+        bigliettoElement.innerHTML = `
+               <div class="card p-3 bg-primary-subtle">
+                  <table class="table table table-bordered">
+                     <thead>
+                        <tr>
+                           <th scope="col">Km percorsi</th>
+                           <th scope="col">Prezzo base</th>
+                           <th scope="col">Sconto applicato</th>
+                           <th scope="col">Prezzo Finale</th>
+                        </tr>
+                     </thead>
+                     <tbody>
+                        <tr>
+                           <td> ${km} km </td>
+                           <td> ${price.toFixed(2)} &#8364</td>
+                           <td>20%</td>
+                           <td> ${discountPrice.toFixed(2)} &#8364 </td>
+                        </tr>
+                     </tbody>
+                  </table>
+               </div>`
+   } else if (age == 2){
     const discount20 = price * 0.20 //number
     let discountPrice = price - discount20 //number
-    bigliettoElement.innerHTML = `<div>Il costo del biglietto è di ${discountPrice.toFixed(2)} &#8364 al quale è stato applicato uno sconto del 40% </div>`
-
+    bigliettoElement.innerHTML = `<div class="col">
+    <div class="card p-3 bg-primary-subtle">
+       <table class="table table table-bordered">
+          <thead>
+             <tr>
+                <th scope="col">Km percorsi</th>
+                <th scope="col">Prezzo base</th>
+                <th scope="col">Sconto applicato</th>
+                <th scope="col">Prezzo Finale</th>
+             </tr>
+          </thead>
+          <tbody>
+             <tr>
+                <td> ${km} km </td>
+                <td> ${price.toFixed(2)} &#8364</td>
+                <td>40%</td>
+                <td> ${discountPrice.toFixed(2)} &#8364 </td>
+             </tr>
+          </tbody>
+       </table>
+    </div>
+ </div>`
    } else {
-    bigliettoElement.innerHTML = `<div>Il costo del biglietto è di ${price.toFixed(2)} &#8364 al quale non è stato applicato nessuno sconto</div>`
-
+    bigliettoElement.innerHTML = `<div class="col">
+    <div class="card p-3 bg-primary-subtle">
+       <table class="table table table-bordered">
+          <thead>
+             <tr>
+                <th scope="col">Km percorsi</th>
+                <th scope="col">Prezzo base</th>
+                <th scope="col">Sconto applicato</th>
+                <th scope="col">Prezzo Finale</th>
+             </tr>
+          </thead>
+          <tbody>
+             <tr>
+                <td> ${km} km </td>
+                <td> ${price.toFixed(2)} &#8364</td>
+                <td>Nessuno sconto</td>
+                <td> ${price.toFixed(2)} &#8364 </td>
+             </tr>
+          </tbody>
+       </table>
+    </div>
+ </div>`
    }
 })
